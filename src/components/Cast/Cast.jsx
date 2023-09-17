8a3e5bb6e9d1628bf4c9faeb748a6f47d117cc8a
AntonTy35 <antony.tislenko@gmail.com>
2023-09-17 03:22:18 +0300

    cast

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -13,27 +13,25 @@ const Cast = () => {
 
   return (
     <Ul>
-      {cast.length > 0 ? (
-        cast.map(({ id, name, profile_path, character }) => (
-          <Li key={id}>
-            <img
-              src={
-                profile_path
-                  ? `https://image.tmdb.org/t/p/w500${profile_path}`
-                  : `https://placehold.jp/24/cccccc/ffffff/140x210.png?text=There is no photo`
-              }
-              alt="actor"
-              loading="lazy"
-              width={120}
-              height={180}
-            />
-            <h4>{name}</h4>
-            <p>Character: {character}</p>
-          </Li>
-        ))
-      ) : (
-        <p>We don't have any cast for this movie.</p>
-      )}
+      {cast.length > 0
+        ? cast.map(({ id, name, profile_path, character }) => (
+            <Li key={id}>
+              <img
+                src={
+                  profile_path
+                    ? `https://image.tmdb.org/t/p/w500${profile_path}`
+                    : `https://placehold.jp/24/cccccc/ffffff/140x210.png?text=There is no photo`
+                }
+                alt="actor"
+                loading="lazy"
+                width={120}
+                height={180}
+              />
+              <h4>{name}</h4>
+              <p>Character: {character}</p>
+            </Li>
+          ))
+        : "We don't have any cast for this movie."}
     </Ul>
   );
 };
